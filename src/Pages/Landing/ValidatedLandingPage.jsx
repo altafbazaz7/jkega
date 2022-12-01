ebf5de98be32d8e3d92d210f3554052bcb4ed785
Mohammad Altaf Bazaz <86340586+altafbazaz7@users.noreply.github.com>
2022-12-01 23:59:30 +0530

    new

--- src/Pages/Landing/ValidatedLandingPage.jsx
+++ src/Pages/Landing/ValidatedLandingPage.jsx
@@ -3,22 +3,22 @@ import { useNavigate } from 'react-router-dom';
 import { useUserAuth } from '../../Context/UserAuthContext';
 
 const ValidatedLandingPage = () => {
-  const { logOut, user } = useUserAuth();
-  const navigate = useNavigate();
-  const handleLogout = async () => {
-    try {
-      await logOut();
-      navigate("/");
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-  return (
-    <div>
-      Welcome User ! {user && user.email}
-      <button onClick={handleLogout}>Logout</button>
-    </div>
-  )
+    const { logOut, user } = useUserAuth();
+    const navigate = useNavigate();
+    const handleLogout = async() => {
+        try {
+            await logOut();
+            navigate("/");
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+    return ( <
+        div >
+        Welcome User!{ user && user.email } <
+        button onClick = { handleLogout } > Logout < /button> < /
+        div >
+    )
 }
 
 export default ValidatedLandingPage
